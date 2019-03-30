bfcfa635ff90f97a598c913602aafc973e590221
Gaurav0812 <gaurav16t@gmail.com>
2019-03-30 18:26:56 +0530

    1st Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReceiptComponent } from './receipt/receipt.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReceiptComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
